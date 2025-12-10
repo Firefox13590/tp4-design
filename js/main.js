3a901aef3c2f69a4e8adc0a38990e64d44817c37
AnaThePineapple <119064232+AnaThePineapple@users.noreply.github.com>
2025-12-10 10:36:19 -0500

    merged everything neatly
    poggers

--- js/main.js
+++ js/main.js
@@ -9,12 +9,19 @@ function OuvrirFermerMenu(evt) {
   let cibleClic = evt.target;
   let barre2 = document.querySelector(".barre2")
   let barre3 = document.querySelector(".barre3")
+  let liensMenus = document.querySelectorAll("header nav a")
   if (!barre3.classList.contains("actif")) {
     barre2.classList.add("actif")
     barre3.classList.add("actif")
+    for (const lien of liensMenus) {
+      lien.classList.add("actif") 
+    }
   } else {
     barre2.classList.remove("actif")
     barre3.classList.remove("actif")
+    for (const lien of liensMenus) {
+      lien.classList.remove("actif") 
+    }
   }
   
 }
