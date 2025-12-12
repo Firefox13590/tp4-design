ba9ba5f35d039e903e82b8093ea642e1ae17ed5d
Firefox13590 <pharlyb@gmail.com>
2025-12-11 20:09:05 -0500

    enfin fini

--- js/rdv.js
+++ js/rdv.js
@@ -4,21 +4,30 @@
 const 
 datepickerJours = document.querySelector("#jours"),
 timepicker = document.querySelector("#timepicker"),
-// /** @type {NodeListOf<HTMLButtonElement>} */
-// btnsNavigationForm = document.querySelectorAll("#navigation-form button"),
 btnsNavigationForm = document.querySelectorAll("button"),
 sectionsForm = document.querySelectorAll("fieldset"),
-barreProgression = document.querySelector("div#barre-progression > div");
-console.log(datepickerJours, timepicker, btnsNavigationForm, sectionsForm, barreProgression);
+barreProgression = document.querySelector("div#barre-progression > div"),
+choixJours = document.querySelectorAll("#jours .datepicker-item"),
+choixHeures = document.querySelectorAll(".timepicker-item");
+console.log(datepickerJours);
+console.log(timepicker);
+console.log(btnsNavigationForm);
+console.log(sectionsForm);
+console.log(barreProgression);
+console.log(choixJours);
+console.log(choixHeures);
 
 let
-progressionForm = 0,
-maxFormSteps = 4;
+progressionForm = 0;
+// maxFormSteps = 4;
 
 
 
 /* 
             FONCTIONS
+ */
+/**
+ * Met à jour l'affichage du formulaire en fonction de la progression
  */
 function updateFormDisplay(){
     sectionsForm.forEach((elem, index) => {
@@ -31,16 +40,17 @@ function updateFormDisplay(){
 
     if(progressionForm == 0){
         btnsNavigationForm[0].disabled = true;
-    }else if(progressionForm == maxFormSteps){
+    }else if(progressionForm == 4){
         btnsNavigationForm[1].disabled = true;
     }else{
         btnsNavigationForm.forEach(elem => elem.disabled = false);
     }
 
-    barreProgression.style.width = `${progressionForm * (100 / maxFormSteps)}%`;
+    barreProgression.style.width = `${progressionForm * (25)}%`;
 }
 
 /**
+ * Navigue dans le formulaire
  * 
  * @param {Number} variation 
  */
@@ -49,6 +59,19 @@ function navigateForm(variation){
     progressionForm += variation;
     // console.log(progressionForm);
     updateFormDisplay();
+}
+
+/**
+ * Gère la sélection d'un choix dans le formulaire (jour ou heure)
+ * 
+ * @param {HTMLElement} parent Conteneur parent des choix
+ * @param {HTMLElement} elem Élément cliqué
+ */
+function selectChoice(parent, elem){
+    for(const child of parent.children){
+        child.classList.remove("selected");
+    }
+    elem.classList.add("selected");
 }
 
 
@@ -61,19 +84,21 @@ for(i = 1; i < 32; i++){
     let nouvSpan = document.createElement("span");
     nouvSpan.classList.add("datepicker-item");
     nouvSpan.innerHTML = i;
+    nouvSpan.addEventListener("click", () =>{
+        selectChoice(datepickerJours, nouvSpan);
+    });
     datepickerJours.append(nouvSpan);
 }
 for(i = 8; i < 16; i++){
     let nouvSpan = document.createElement("span");
     nouvSpan.classList.add("timepicker-item");
     nouvSpan.innerHTML = `${i}h-${i + 1}h`;
+    nouvSpan.addEventListener("click", () =>{
+        selectChoice(timepicker, nouvSpan);
+    });
     timepicker.append(nouvSpan);
 }
 
-// event click sur les btns
-// btnsNavigationForm.forEach((elem, index) => {
-//     elem.addEventListener("click", navigateForm(index * 2 - 1));
-// });
 
 
 
